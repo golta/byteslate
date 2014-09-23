4a38072b5ddaf7c0d4d2f107daac67c0f4ef8072
apexkid <apexkid@github.com>
2014-09-24 05:25:51 +0530

    minor typo fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import bootstrap
-from flask.ext.mail import mail
-from flask.ext.moment import moment
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
